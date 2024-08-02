46628d7b379638d6c1003104da02107ef5acfa77
KevinDylanPrayudi <kevin.dylan.prayudi20@gmail.com>
2024-08-02 18:12:02 +0700

    feat(swagger): adding API documentation of login

--- routes/api/v1/login/index.js
+++ routes/api/v1/login/index.js
@@ -4,6 +4,36 @@ const model = require('./model');
 const { validator} = require('./validator');
 
 function main(db) {
+
+    /**
+     * @swagger
+     * /login:
+     *   post:
+     *     tags:
+     *      - Login
+     *     summary: Login
+     *     requestBody:
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             $ref: 'http://localhost:3000/public/json/requests/login.json#/post:login'
+     *     responses:
+     *       201:
+     *         content:
+     *           application/json:
+     *             schema:
+     *               $ref: 'http://localhost:3000/public/json/responses/login.json#/post:login'
+     *       400:
+     *         description: Please check schema tab for more details kind of error
+     *         content:
+     *           application/json:
+     *             schema:
+     *               oneOf:
+     *               - $ref: 'http://localhost:3000/public/json/responses/login.json#/post:login:validation'
+     *               - $ref: 'http://localhost:3000/public/json/responses/login.json#/post:login:bad-request'
+     */
+
     async function post(req, res) {
         try {
             await validator().post().validateAsync(req.body);
